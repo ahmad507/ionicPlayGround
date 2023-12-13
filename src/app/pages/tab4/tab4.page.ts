2138618d56af87d9620547bf9fdf57e322109fdf
ahmad507 <ahmad.94192@gmail.com>
2023-12-13 16:51:31 +0700

    STG-001(App.ts)Initial Commit
    Signed-off-by: ahmad507 <ahmad.94192@gmail.com>

---
+++ src/app/pages/tab4/tab4.page.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule } from '@ionic/angular';
+import {ExploreContainerComponent} from "../../components/explore-container/explore-container.component";
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule, ExploreContainerComponent]
+})
+export class Tab4Page {
+  constructor() {
+
+  }
+}
